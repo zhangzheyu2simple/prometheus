b8fce4210c058144778000321820b684697e2f82
Callum Styan <callumstyan@gmail.com>
2019-12-31 14:20:25 -0800

    Take num of samples to expect in expectSampleCount instead of array of samples.
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- storage/remote/queue_manager_test.go
+++ storage/remote/queue_manager_test.go
@@ -399,10 +399,10 @@ func (c *TestStorageClient) waitForExpectedSamples(tb testing.TB) {
 	}
 }
 
-func (c *TestStorageClient) expectSampleCount(ss []record.RefSample) {
+func (c *TestStorageClient) expectSampleCount(numSamples int) {
 	c.mtx.Lock()
 	defer c.mtx.Unlock()
-	c.wg.Add(len(ss))
+	c.wg.Add(numSamples)
 }
 
 func (c *TestStorageClient) waitForExpectedSampleCount() {
@@ -508,7 +508,7 @@ func BenchmarkSampleDelivery(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		c.expectSampleCount(samples)
+		c.expectSampleCount(len(samples))
 		m.Append(samples)
 		c.waitForExpectedSampleCount()
 	}
